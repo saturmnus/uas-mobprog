18d40ca6abda9c1708538d93b6adcf4b1be54bc3
Salma <salmanadya686@gmail.com>
2025-01-06 22:10:57 +0700

    uas mobprog

--- app/src/main/java/com/salma/basjep/HomeActivity.java
+++ app/src/main/java/com/salma/basjep/HomeActivity.java
@@ -1,24 +1,59 @@
 package com.salma.basjep;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button buttonHiragana = findViewById(R.id.button2);
+        Button buttonKatakana = findViewById(R.id.button3);
+        Button buttonBuahBuahan = findViewById(R.id.button4);
+        Button buttonHewan = findViewById(R.id.button5);
+        Button buttonPercakapan = findViewById(R.id.button6);
+        Button buttonRuangPublik = findViewById(R.id.button7);
+        Button buttonQuiz = findViewById(R.id.button8);
+
+        buttonHiragana.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, HiraganaActivity.class);
+            startActivity(intent);
+        });
+
+        buttonKatakana.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, KatakanaActivity.class);
+            startActivity(intent);
+        });
+
+        buttonBuahBuahan.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, BuahActivity.class);
+            startActivity(intent);
+        });
+
+        buttonHewan.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, HewanActivity.class);
+            startActivity(intent);
+        });
+
+        buttonPercakapan.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, PercakapanActivity.class);
+            startActivity(intent);
+        });
+
+        buttonRuangPublik.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, PublikActivity.class);
+            startActivity(intent);
+        });
+
+        buttonQuiz.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, QuizActivity.class);
+            startActivity(intent);
         });
     }
 }
